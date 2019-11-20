6782d6fdbd9b517e934af3944d338fa52b9999f1
Miles Bradt <smbradtmichael@gmail.com>
2019-11-20 08:57:35 -0800

    fix title

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'template.ejs',
       appMountId: 'react-app-root',
-      title: 'Miles Portfolio',
+      title: 'Tamagotchi',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
